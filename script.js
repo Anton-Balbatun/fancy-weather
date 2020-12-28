76fa435830dca113105c7b1ab2ed1cb8a59d977f
Anton-Balbatun <anton553322@gmail.com>
2020-12-29 01:04:58 +0300

    Add try catch constructions

--- script.js
+++ script.js
@@ -36,8 +36,18 @@ function changeBackgroundImg(){
 async function getcurrentPosition(){
      
         let url = `https://ipinfo.io/json?token=3bd29cca703424` 
-        let response = await fetch(url) 
-        let data = await response.json()
+
+        try {
+
+            let response = await fetch(url) 
+            var data = await response.json()
+            
+        } catch (e) {
+
+            alert( ` Извините,произошла ошибка, Name: ${e.name} Message: ${e.message} ` );
+
+        }
+        
         let arr = data.loc.split(',').reverse()
         let Longitude  = arr[0]
         let Latitude = arr[1]
@@ -57,8 +67,17 @@ async function getPosition(){
     console.log( document.querySelector('#Lattitude'))
     let city = document.querySelector('.citySearch').value
     let url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}.json?access_token=pk.eyJ1IjoiYW50b241NTMzMjIiLCJhIjoiY2thdXZmbDRoMDV6YzJ4dTk3Ymk5b3E4dyJ9.mdkX1Z26DQVJEa54fEEGTA`
-    let response = await fetch(url) 
-    let data = await response.json()
+    try {
+
+        let response = await fetch(url) 
+        var data = await response.json()
+        
+    } catch (e) {
+
+        alert( ` Извините,произошла ошибка, Name: ${e.name} Message: ${e.message} ` );
+        
+    }
+
     let arr = data.features[0].center
     let Latitude = arr[0]
     let Longitude = arr[1]
@@ -86,8 +105,17 @@ async function getPosition(){
 async function getCurrentWeather(lat,lon){
 
     let url = `https://api.weatherapi.com/v1/forecast.json?key=0c6ecd00b4c34857933173038203105&q=${lat},${lon}&days=3` 
-    let response = await fetch(url) 
-    let data = await response.json()
+
+    try {
+
+        let response = await fetch(url) 
+        var data = await response.json()
+        
+    } catch (e) {
+        
+        alert( ` Извините,произошла ошибка, Name: ${e.name} Message: ${e.message} ` );
+        
+    }
     createWeather(data)
 
 }
